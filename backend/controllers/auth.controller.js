3e7d0642a74001040bf1b5e6d3b3ffd00c24392e
kishumar <166789760+kishumar@users.noreply.github.com>
2026-04-01 14:12:14 +0530

    Update auth.controller.js
    1 update

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -17,8 +17,8 @@ export const googleAuth= async (req, res)=>{
 
         res.cookie("token",token,{
             httpOnly:true,
-            secure:false,
-            sameSite:"strict",
+            secure:true,
+            sameSite:"none",
             maxAge:7*24*60*60*1000
         }
         )
@@ -34,8 +34,8 @@ export const logOut = async(req, res)=>{
 try {
      res.clearCookie("token",{
      httpOnly:true,
-     secure:false,
-     sameSite:"strict"   
+     secure:true,
+     sameSite:"none"   
     })
     return res.status(200).json({message: "logout successfully"})
 } catch (error) {
